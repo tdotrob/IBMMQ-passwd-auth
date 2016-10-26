644756e33ef40b90e405d997558f771048bfbf89
FJ Brandelik <fjb_saper@msn.com>
2016-10-26 15:32:38 -0400

    added listener, changed channel to pass user from cmdline fixes #1

--- Tools/bat/chlauthmenu.bat
+++ Tools/bat/chlauthmenu.bat
@@ -8,35 +8,47 @@
 :: 
 :: 
 :: ============================================================================
-:: 20161017 T.Rob    New script
-:: 
+:: 20161017 T.Rob    	New script
+:: 20161026 fjbsaper	made elevated privilege independant of MQ
+::                      changed testuser to %testuser%
 :: 
 :: ============================================================================
 
 
-:: Arrange to discard command line history from the menu keystrokes
 @echo off
-if "%~1"==":historySafe" goto :historySafe
-cmd /d /c "%~f0" :historySafe
+:: check parameters
+set testuser=%1
+if "a%testuser%a" == "aa" (
+	goto usage
+)
+
+
+:: Arrange to discard command line history from the menu keystrokes
+if "%~2"==":historySafe" goto :historySafe
+cmd /d /c "%~f0" %1 :historySafe
 exit /b
 
 :historySafe
+echo testuser=%testuser%
 
 :: Main program begins
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
 SETLOCAL
 
-:: Test to see if user has WMQ admin privileges
-set Response=
-set isAdmin=
-for /f "tokens=*" %%a in ('dspmqtrn 2^>^&1') do @set Response=%%a
 
-set isAdmin=!Response:~0,7!
+:: Test to see if user has elevated WMQ admin privileges
+:: we are running strmqm which does require elevated privileges in windows
+:: set Response=
+:: for /f "tokens=*" %%a in ('dspmqtrn 2^>^&1') do @set Response=%%a
+:: set isAdmin=!Response:~0,7!
+:: if NOT "%isAdmin%"=="AMQ7028" (
 
-if NOT "%isAdmin%"=="AMQ7028" (
+net session > nul 2>&1
+set /a isAdmin=%errorlevel%
+if %isAdmin% gtr 0 (
 	echo. 
-	echo %0: This script must be run from an account with WeSphere MQ adminstrator privileges.
+	echo %0: This script must be run from an account with elevated adminstrator privileges ^(run as administrator^).
 	exit /B 2
 )
 
@@ -58,7 +70,7 @@ echo      !C!3) USERSRC(NOACCESS) - CSP
 echo      !D!4) USERSRC(NOACCESS) - CLNT
 echo       Sets USERSRC(CHL) globally then overrides USERMAP
 echo.
-echo      !E!5) MCAUSER('*NOBODY')
+echo      !E!5) MCAUSER('nobody')
 echo      !F!6) MCAUSER(' ')
 echo.
 echo      !G!7) ADOPTCTX(YES)
@@ -80,13 +92,13 @@ GOTO MENU
 :CASE_1
 	echo SET CHLAUTH^('CHLAUTH.ADDRMAP'^) TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('CHLAUTH.PEERMAP'^) TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^) 	USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
 
 	echo SET CHLAUTH^('COMPAT.ADDRMAP'^)  TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('COMPAT.PEERMAP'^)  TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
 	SET "A=*" & SET "B= " & SET "C= " & SET "D= "
 	timeout /T !TIMR! >nul
 	GOTO END_CASE
@@ -94,13 +106,13 @@ GOTO MENU
 :CASE_2
 	echo SET CHLAUTH^('CHLAUTH.ADDRMAP'^) TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) MCAUSER^('addrmap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('CHLAUTH.PEERMAP'^) TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    MCAUSER^('peermap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('testuser'^) MCAUSER^('usermap2'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    MCAUSER^('usermap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) MCAUSER^('usermap2'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    MCAUSER^('usermap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
 
 	echo SET CHLAUTH^('COMPAT.ADDRMAP'^)  TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) MCAUSER^('addrmap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('COMPAT.PEERMAP'^)  TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    MCAUSER^('peermap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('testuser'^) MCAUSER^('usermap2'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    MCAUSER^('usermap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) MCAUSER^('usermap2'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    MCAUSER^('usermap1'^) USERSRC^(MAP^) CHCKCLNT^(REQUIRED^) ACTION^(REPLACE^) | runmqsc ASH
 	SET "A= " & SET "B=*" & SET "C= " & SET "D= "
 	timeout /T !TIMR! >nul
 	GOTO END_CASE
@@ -108,17 +120,17 @@ GOTO MENU
 :CASE_3
 	echo SET CHLAUTH^('CHLAUTH.ADDRMAP'^) TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('CHLAUTH.PEERMAP'^) TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 
 	echo SET CHLAUTH^('COMPAT.ADDRMAP'^)  TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('COMPAT.PEERMAP'^)  TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
 
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 	SET "A=*" & SET "B= " & SET "C=*" & SET "D= "
 	timeout /T !TIMR! >nul
 	GOTO END_CASE
@@ -130,27 +142,31 @@ GOTO MENU
 	echo SET CHLAUTH^('COMPAT.ADDRMAP'^)  TYPE^(ADDRESSMAP^) ADDRESS^('127.0.0.1'^) USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 	echo SET CHLAUTH^('COMPAT.PEERMAP'^)  TYPE^(SSLPEERMAP^) SSLPEER^('CN=mqm'^)    USERSRC^(CHANNEL^)  CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('testuser'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
+	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('%testuser%'^) USERSRC^(CHANNEL^) CHCKCLNT^(REQUIRED^)                   ACTION^(REPLACE^) | runmqsc ASH
 
-	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
-	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('CHLAUTH.USERMAP'^) TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
+::	echo SET CHLAUTH^('COMPAT.USERMAP'^)  TYPE^(USERMAP^)    CLNTUSER^('T.Rob'^)    USERSRC^(NOACCESS^) CHCKCLNT^(REQUIRED^)                  ACTION^(REPLACE^) | runmqsc ASH
 	SET "A=*" & SET "B= " & SET "C= " & SET "D=*"
 	timeout /T !TIMR! >nul
 	GOTO END_CASE
 
 :CASE_5
-	echo DEFINE CHANNEL^('CHLAUTH.ADDRMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^('^*NOBODY'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
-	echo DEFINE CHANNEL^('CHLAUTH.PEERMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^('^*NOBODY'^) SSLCIPH^('TLS_RSA_WITH_3DES_EDE_CBC_SHA'^) TRPTYPE^(TCP^) REPLACE | runmqsc ASH
-	echo DEFINE CHANNEL^('CHLAUTH.USERMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^('^*NOBODY'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
-	echo DEFINE CHANNEL^('COMPAT.ADDRMAP'^)  CHLTYPE^(SVRCONN^) MCAUSER^('^*NOBODY'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
-	echo DEFINE CHANNEL^('COMPAT.PEERMAP'^)  CHLTYPE^(SVRCONN^) MCAUSER^('^*NOBODY'^) SSLCIPH^('TLS_RSA_WITH_3DES_EDE_CBC_SHA'^) TRPTYPE^(TCP^) REPLACE | runmqsc ASH
-	echo DEFINE CHANNEL^('COMPAT.USERMAP'^)  CHLTYPE^(SVRCONN^) MCAUSER^('^*NOBODY'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
+	echo define LISTENER^(ASH.TCP.1416^) trptype^(TCP^) control^(qmgr^) port^(1416^) REPLACE | runmqsc ASH
+	echo start LISTENER^(ASH.TCP.1416^) | runmqsc ASH
+	echo DEFINE CHANNEL^('CHLAUTH.ADDRMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^('^*nobody'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
+	echo DEFINE CHANNEL^('CHLAUTH.PEERMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^('^*nobody'^) SSLCIPH^('TLS_RSA_WITH_3DES_EDE_CBC_SHA'^) TRPTYPE^(TCP^) REPLACE | runmqsc ASH
+	echo DEFINE CHANNEL^('CHLAUTH.USERMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^('^*nobody'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
+	echo DEFINE CHANNEL^('COMPAT.ADDRMAP'^)  CHLTYPE^(SVRCONN^) MCAUSER^('^*nobody'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
+	echo DEFINE CHANNEL^('COMPAT.PEERMAP'^)  CHLTYPE^(SVRCONN^) MCAUSER^('^*nobody'^) SSLCIPH^('TLS_RSA_WITH_3DES_EDE_CBC_SHA'^) TRPTYPE^(TCP^) REPLACE | runmqsc ASH
+	echo DEFINE CHANNEL^('COMPAT.USERMAP'^)  CHLTYPE^(SVRCONN^) MCAUSER^('^*nobody'^) TRPTYPE^(TCP^)                                            REPLACE | runmqsc ASH
 	SET "E=*" & SET "F= "
 	timeout /T !TIMR! >nul
 	GOTO END_CASE
 
 :CASE_6
+	echo define LISTENER^(ASH.TCP.1416^) trptype^(TCP^) control^(qmgr^) port^(1416^) REPLACE | runmqsc ASH
+	echo start LISTENER^(ASH.TCP.1416^) | runmqsc ASH
 	echo DEFINE CHANNEL^('CHLAUTH.ADDRMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^(' '^) TRPTYPE^(TCP^)                                                 REPLACE | runmqsc ASH
 	echo DEFINE CHANNEL^('CHLAUTH.PEERMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^(' '^) SSLCIPH^('TLS_RSA_WITH_3DES_EDE_CBC_SHA'^) TRPTYPE^(TCP^)      REPLACE | runmqsc ASH
 	echo DEFINE CHANNEL^('CHLAUTH.USERMAP'^) CHLTYPE^(SVRCONN^) MCAUSER^(' '^) TRPTYPE^(TCP^)                                                 REPLACE | runmqsc ASH
@@ -171,17 +187,17 @@ GOTO MENU
 :CASE_8
 	echo ALTER AUTHINFO^('SYSTEM.DEFAULT.AUTHINFO.IDPWOS'^) AUTHTYPE^(IDPWOS^) CHCKCLNT^(REQUIRED^) ADOPTCTX^(NO^) | runmqsc ASH
 	echo REFRESH SECURITY TYPE^(CONNAUTH^) | runmqsc ASH
-	SE	T "G= " & SET "H=*"
+	SET "G= " & SET "H=*"
 	timeout /T !TIMR! >nul
 	GOTO END_CASE
 	
 :CASE_R
 :CASE_r
 	echo ALTER QMGR AUTHOERV(ENABLED) CHLEV(ENABLED) SSLEV(ENABLED)                       | runmqsc ASH 1>NUL 2>&1
-	echo SET CHLAUTH('CHLAUTH.*'^) TYPE(BLOCKUSER^) USERLIST('*NOBODY'^) ACTION(REPLACE^) | runmqsc ASH 1>NUL 2>&1
-	echo SET CHLAUTH('COMPAT.*'^)  TYPE(BLOCKUSER^) USERLIST('*NOBODY'^) ACTION(REPLACE^) | runmqsc ASH 1>NUL 2>&1
+	echo SET CHLAUTH('CHLAUTH.*'^) TYPE(BLOCKUSER^) USERLIST('nobody'^) ACTION(REPLACE^) | runmqsc ASH 1>NUL 2>&1
+	echo SET CHLAUTH('COMPAT.*'^)  TYPE(BLOCKUSER^) USERLIST('nobody'^) ACTION(REPLACE^) | runmqsc ASH 1>NUL 2>&1
 	SET TIMR2=!TIMR!
-	SET TIMR=0
+	SET /a TIMR=0
 	2>NUL CALL :CASE_1
 	2>NUL CALL :CASE_5
 	SET TIMR=!TIMR2!
@@ -195,3 +211,13 @@ GOTO MENU
 :END_CASE
 	VER > NUL # reset ERRORLEVEL
 	GOTO :EOF # return from CALL
+
+goto EOF
+
+:usage
+echo Usage is :
+echo %0 testuserid
+echo example: %0 T.Rob
+exit /b 4
+
+:EOF
